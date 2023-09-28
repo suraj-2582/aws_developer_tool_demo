7c9360caaa039864ef9a1103a750d719ff07a2fa
suraj-2582 <144020761+suraj-2582@users.noreply.github.com>
2023-09-28 13:46:03 +0530

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,5 +1,5 @@
 import requests
 
 def test_index():
-    response = requests.get("http://13.229.200.22:5000/")
+    response = requests.get("http://13.229.200.22:8080/")
     assert response.status_code == 200
