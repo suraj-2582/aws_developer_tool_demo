40dac047d7ac282bc882fe2d5e72f8986edd7f2a
suraj-2582 <144020761+suraj-2582@users.noreply.github.com>
2023-09-28 12:51:52 +0530

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,5 +1,5 @@
 import requests
 
 def test_index():
-    response = requests.get("http://127.0.0.1:5000/")
+    response = requests.get("http://172.31.40.146:5000/")
     assert response.status_code == 200
